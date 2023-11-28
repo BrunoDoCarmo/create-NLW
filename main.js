caa79a46faf3bcead233b0279d9085736e5b454a
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2023-11-28 13:22:21 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'BrunoDoCarmo',
+  youtube: 'jakelinygracielly',
+  facebook: 'maykbrito',
+  instagram: 'jakeliny.gracielly',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      userBio.textContent = data.bio
+      userImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
